956e67a2eb239e4cdd319dd3f4f11e884160f368
yusupha darboe <darboeyusuf22@gmail.com>
2025-04-17 22:03:12 +0000

    payroll

--- routers/payRoll.js
+++ routers/payRoll.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { createPayRoll ,getEmployeeSalary,getAllEmployeeSalary,deletePayroll, getEmailText} from '../controllers/payRollController.js';
+import { authenticateJWT,isAdmin } from '../middlewares/verifyJwt.js';
 
 const router=express.Router()
 
-router.post('/addPayroll',createPayRoll)
-router.get('/singlePayroll/:employee_id',getEmployeeSalary)
-router.get('/payroll_list',getAllEmployeeSalary)
-router.delete('/payroll_delete/:id',deletePayroll)
-router.get('/message',getEmailText)
+router.post('/addPayroll',createPayRoll,authenticateJWT,isAdmin)
+router.get('/singlePayroll/:employee_id',getEmployeeSalary,authenticateJWT,isAdmin)
+router.get('/payroll_list',getAllEmployeeSalary,authenticateJWT,isAdmin)
+router.delete('/payroll_delete/:id',deletePayroll,authenticateJWT,isAdmin)
+router.get('/message',getEmailText,authenticateJWT,isAdmin)
 
 export { router as payrollRouter};
