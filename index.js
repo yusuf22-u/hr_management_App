d707352246f218f9b0a3e069787b8e12882d655b
yusupha darboe <darboeyusuf22@gmail.com>
2025-04-21 15:39:26 +0000

    payroll function

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
     cors: {
         origin: 'https://hr-management-sys-app.netlify.app', // Allow this origin
-        // origin: 'http://localhost:5173', // Allow this origin
+       
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
         credentials: true // Allow credentials (cookies, authorization headers)
     }
@@ -53,8 +53,7 @@ export { io };
 // Middleware setup
 app.use(cookieParser());
 app.use(cors({
-      origin: 'https://hr-management-sys-app.netlify.app',
-    // origin: 'http://localhost:5173',
+    origin: 'https://hr-management-sys-app.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
